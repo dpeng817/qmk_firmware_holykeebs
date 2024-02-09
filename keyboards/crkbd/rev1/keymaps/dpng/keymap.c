588b2dd99c300b99294283faa1e103911afe1c64
Chris DeCarolis <cdguitar817@gmail.com>
2024-02-08 22:40:20 -0800

    Add a button to switch mouse behaviors

--- keyboards/crkbd/rev1/keymaps/dpng/keymap.c
+++ keyboards/crkbd/rev1/keymaps/dpng/keymap.c
@@ -13,10 +13,11 @@ enum custom_keycodes {
   M_ALFRED, // Opens alfred in arc mode
   M_HMRW, // Opens homerow
   M_SCRSHT, // Activates screenshot mode on mac
-  M_ARROW, // Types pythonic arrow
+  M_SWITCH_MOUSE, // Switches mouse behavior
 };
 
-
+// For mouse switching behavior
+bool mouse_switch = false;
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   static uint16_t tab_l3_timer;
@@ -90,11 +91,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         SEND_STRING(SS_LGUI(SS_LSFT("4")));
       }
       return false;
-    // Types pythonic arrow
-    case M_ARROW:
+    case M_SWITCH_MOUSE:
       if (record->event.pressed) {
-        SEND_STRING("->");
+        mouse_switch = !mouse_switch;
       }
+      return true;
   }
   return true;
 }
@@ -106,28 +107,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,    KC_Q,       KC_W,       KC_E,       KC_R,        KC_T,                KC_Y,     KC_U,       KC_I,       KC_O,      KC_P,     KC_BSPC,
     KC_ESC,    KC_A,       KC_S,       KC_D,       KC_F,        KC_G,                KC_H,     KC_J,       KC_K,       KC_L,      KC_SCLN,  KC_QUOT,
     KC_LSFT,   KC_Z,       KC_X,       KC_C,       KC_V,        KC_B,                KC_N,     KC_M,       KC_COMM,    KC_DOT,    KC_SLSH,  KC_ENTER,
-                                       KC_LGUI,    MO(2),       KC_BTN1,             KC_NO,    KC_SPC,     MO(1)                        
+                                       KC_LGUI,    MO(1),       KC_BTN1,             KC_NO,    KC_SPC,     MO(2)                        
   ),
   // General use macros, numbers
 	[1] = LAYOUT_split_3x6_3(
-    KC_TAB,    KC_1,       KC_2,       KC_3,       QK_CLEAR_EEPROM,       KC_NO,               KC_NO,    KC_NO,       KC_NO,      KC_NO,    KC_NO,    KC_BSPC,
+    KC_TAB,    KC_1,       KC_2,       KC_3,       QK_CLEAR_EEPROM,       M_SWITCH_MOUSE,               KC_NO,    KC_NO,       KC_NO,      KC_NO,    KC_NO,    KC_BSPC,
     KC_ESC,    KC_4,       KC_5,       KC_6,       M_ALFRED,    M_HMRW,              KC_NO,    KC_NO,       KC_NO,      KC_NO,    KC_NO,    KC_NO,
-    KC_LSFT,   KC_7,       KC_8,       KC_9,       M_SCRSHT,    M_ARROW,             KC_NO,    KC_NO,       KC_NO,      KC_NO,    KC_NO,    KC_LCTL,
-                                       KC_0,       MO(2),       KC_BTN2,             KC_NO,    KC_SPC,      MO(1)                          
+    KC_LSFT,   KC_7,       KC_8,       KC_9,       M_SCRSHT,    KC_NO,             KC_NO,    KC_NO,       KC_NO,      KC_NO,    KC_NO,    KC_LCTL,
+                                       KC_0,       MO(1),       KC_BTN2,             KC_NO,    KC_SPC,      MO(2)                          
   ),
   // Programming characters and vim macros
   [2] = LAYOUT_split_3x6_3(
     KC_TAB,    KC_EXLM,    KC_AT,      KC_HASH,    KC_NO,       KC_NO,               KC_EQL,   KC_LCBR,     KC_RCBR,    KC_PIPE,  KC_P,     KC_BSPC,
     KC_ESC,    KC_HASH,    KC_PERC,    KC_CIRC,    M_ALFRED,    M_HMRW,              KC_MINS,  KC_LBRC,     KC_RBRC,    KC_BSLS,  KC_SCLN,  KC_DQUO,
     KC_LSFT,   KC_AMPR,    KC_ASTR,    KC_NO,      KC_NO,       KC_NO,               KC_UNDS,  KC_LABK,     KC_RABK,    KC_GRAVE, KC_TILD,  KC_ENTER,
-                                       KC_LGUI,    MO(2),       KC_BTN2,             KC_NO,    KC_SPC,      MO(1)                        
+                                       KC_LGUI,    MO(1),       KC_BTN2,             KC_NO,    KC_SPC,      MO(2)                        
   ),
   // ? Haven't figured out what to put here yet. Probably system stuff.
   [2] = LAYOUT_split_3x6_3(
     KC_TAB,    KC_EXLM,    KC_AT,      KC_HASH,    KC_NO,       KC_NO,               KC_EQL,   KC_LCBR,     KC_RCBR,    KC_PIPE,  KC_P,     KC_BSPC,
     KC_ESC,    KC_HASH,    KC_PERC,    KC_CIRC,    KC_NO,       KC_NO,               KC_MINS,  KC_LBRC,     KC_RBRC,    KC_BSLS,  KC_SCLN,  KC_DQUO,
     KC_LSFT,   KC_AMPR,    KC_ASTR,    KC_NO,      KC_NO,       KC_NO,               KC_UNDS,  KC_LABK,     KC_RABK,    KC_GRAVE, KC_TILD,  KC_ENTER,
-                                       KC_LGUI,    MO(2),       KC_BTN2,             KC_NO,    KC_SPC,      MO(1)                        
+                                       KC_LGUI,    MO(1),       KC_BTN2,             KC_NO,    KC_SPC,      MO(2)                        
   ),
 };
 
@@ -156,49 +157,55 @@ int accumulated_arrow_y = 0;
 float average_arrow_x = 0;
 float average_arrow_y = 0;
 
-#define ARROW_MOMENTUM 0.99
+#define ARROW_MOMENTUM 0.01
 
 report_mouse_t pointing_device_task_combined_user(report_mouse_t left_report, report_mouse_t right_report) {
-
-    // Layer not 2: left trackball scroll, right trackball mouse
+    report_mouse_t main_report = right_report;
+    report_mouse_t sub_report = left_report;
+    if (mouse_switch) {
+        main_report = left_report;
+        sub_report = right_report;
+    }
+    
+    // Layer not 2: sub trackball scroll, main trackball mouse
     if (!layer_state_is(2)) {
         // Calculate and accumulate scroll values based on mouse movement and divisors
-        scroll_accumulated_h += (float)left_report.x / SCROLL_DIVISOR_H;
-        scroll_accumulated_v += (float)left_report.y / SCROLL_DIVISOR_V;
+        scroll_accumulated_h += (float)sub_report.x / SCROLL_DIVISOR_H;
+        scroll_accumulated_v += (float)sub_report.y / SCROLL_DIVISOR_V;
 
         // Assign integer parts of accumulated scroll values to the mouse report
-        left_report.h = -(int16_t)scroll_accumulated_h;
-        left_report.v = (int16_t)scroll_accumulated_v;
+        sub_report.h = -(int16_t)scroll_accumulated_h;
+        sub_report.v = (int16_t)scroll_accumulated_v;
 
         // Update accumulated scroll values by subtracting the integer parts
         scroll_accumulated_h -= (int16_t)scroll_accumulated_h;
         scroll_accumulated_v -= (int16_t)scroll_accumulated_v;
 
         // Clear the X and Y values of the mouse report
-        left_report.x = 0;
-        left_report.y = 0;
+        sub_report.x = 0;
+        sub_report.y = 0;
 
         // still treat right trackball behavior as normal.
         // compute the size of the last mouse movement
-        float mouse_length = sqrt(right_report.x*right_report.x + right_report.y*right_report.y);
+        float mouse_length = sqrt(main_report.x*main_report.x + main_report.y*main_report.y);
 
         // compute an instantaneous scaling factor and update exponential moving average
         float factor =  GROWTH_FACTOR*mouse_length+ MIN_SCALE;
         accumulated_factor = accumulated_factor*(1-MOMENTUM) + factor*MOMENTUM;
 
         if (accumulated_factor > MAX_SCALE) {
-            // clamp the scaling factor to avoid overflowing right_report
-            right_report.x *= MAX_SCALE;
-            right_report.y *= MAX_SCALE;
+            // clamp the scaling factor to avoid overflowing main_report
+            main_report.x *= MAX_SCALE;
+            main_report.y *= MAX_SCALE;
         }
         else {
             // scale up the mouse movement by the average factor
-            right_report.x = (int16_t)(right_report.x * accumulated_factor);
-            right_report.y = (int16_t)(right_report.y * accumulated_factor);
+            main_report.x = (int16_t)(main_report.x * accumulated_factor);
+            main_report.y = (int16_t)(main_report.y * accumulated_factor);
         }
     }
 
-    // Layer 2: left trackball arrow keys, right trackball mouse
+    // Layer 2: sub trackball arrow keys, main trackball mouse
     if (layer_state_is(2)) {
         // move one space per click here, biasing towards vertical
         // if you are moving in text horizontally and click an accidental
@@ -215,17 +222,17 @@ report_mouse_t pointing_device_task_combined_user(report_mouse_t left_report, re
         // motion in the direction of the recent average 
 
         // update the accumulated arrow momentum
-        average_arrow_x = average_arrow_x*ARROW_MOMENTUM + (float) left_report.x*(1-ARROW_MOMENTUM);
-        average_arrow_y = average_arrow_y*ARROW_MOMENTUM + (float) left_report.y*(1-ARROW_MOMENTUM);
+        average_arrow_x = average_arrow_x*ARROW_MOMENTUM + (float) sub_report.x*(1-ARROW_MOMENTUM);
+        average_arrow_y = average_arrow_y*ARROW_MOMENTUM + (float) sub_report.y*(1-ARROW_MOMENTUM);
 
         // kill accumulated clicks orthogonal to average direction           
         if (fabs(average_arrow_x) > fabs(average_arrow_y)){
-            accumulated_arrow_x += left_report.x;
+            accumulated_arrow_x += sub_report.x;
             accumulated_arrow_y = 0;
         }
         if (fabs(average_arrow_y) > fabs(average_arrow_x)){
             accumulated_arrow_x = 0;
-            accumulated_arrow_y += left_report.y;
+            accumulated_arrow_y += sub_report.y;
         }
     
         // process queued clicks
@@ -247,30 +254,34 @@ report_mouse_t pointing_device_task_combined_user(report_mouse_t left_report, re
         }
         
         // get a null report for left trackball
-        left_report.x = 0;
-        left_report.y = 0;
+        sub_report.x = 0;
+        sub_report.y = 0;
 
         // still treat right trackball behavior as normal.
         // compute the size of the last mouse movement
-        float mouse_length = sqrt(right_report.x*right_report.x + right_report.y*right_report.y);
+        float mouse_length = sqrt(main_report.x*main_report.x + main_report.y*main_report.y);
 
         // compute an instantaneous scaling factor and update exponential moving average
         float factor =  GROWTH_FACTOR*mouse_length+ MIN_SCALE;
         accumulated_factor = accumulated_factor*(1-MOMENTUM) + factor*MOMENTUM;
 
         if (accumulated_factor > MAX_SCALE) {
-            // clamp the scaling factor to avoid overflowing right_report
-            right_report.x *= MAX_SCALE;
-            right_report.y *= MAX_SCALE;
+            // clamp the scaling factor to avoid overflowing main_report
+            main_report.x *= MAX_SCALE;
+            main_report.y *= MAX_SCALE;
         }
         else {
             // scale up the mouse movement by the average factor
-            right_report.x = (int16_t)(right_report.x * accumulated_factor);
-            right_report.y = (int16_t)(right_report.y * accumulated_factor);
+            main_report.x = (int16_t)(main_report.x * accumulated_factor);
+            main_report.y = (int16_t)(main_report.y * accumulated_factor);
         }
     }
 
-    return pointing_device_combine_reports(left_report, right_report);
+    if (mouse_switch) {
+        return pointing_device_combine_reports(sub_report, main_report);
+    } else {
+        return pointing_device_combine_reports(main_report, sub_report);
+    }
 }
 
 layer_state_t layer_state_set_user(layer_state_t state) {
