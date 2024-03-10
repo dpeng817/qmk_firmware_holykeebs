b0d2955be74581fa6f5bfc201f3ee66cf9b34458
Chris DeCarolis <cdguitar817@gmail.com>
2024-03-09 20:21:41 -0800

    initial keyball44 keymap

--- keyboards/keyball/keyball44/keymaps/dpng/keymap.c
+++ keyboards/keyball/keyball44/keymaps/dpng/keymap.c
@@ -21,35 +21,166 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "quantum.h"
 #include "print.h"
 
+#define BTN_MOUSE LT(0, KC_BTN1)
+#define F_HMRW LT(0, KC_F)
+#define D_ALFD LT(0, KC_D)
+#define C_COPY LT(0, KC_C)
+#define V_PSTE LT(0, KC_V)
+#define W_CLOSE LT(0, KC_W)
+#define ESC_CAPSLOCK LT(0, KC_ESC)
+#define Z_UNDO LT(0, KC_Z)
+#define S_SAVE LT(0, KC_S)
+#define SLSH LT(0, KC_SLSH)
+#define PERC_REDO LT(0, KC_PERC)
+#define LCBR_URL LT(0, KC_LCBR)
+#define SPC_CTRL LT(0, KC_SPC)
+
+enum custom_keycodes {
+  M_SCRSHT = SAFE_RANGE, // Takes a screenshot
+  M_SCRREC, // Starts a screen recording
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case BTN_MOUSE:
+      if(record->tap.count && record->event.pressed) {
+        tap_code16(KC_BTN1);
+      } 
+      else if (record->event.pressed) {
+        tap_code16(KC_BTN2);
+      }
+      return false;
+    // opens alfred in arc mode
+    case D_ALFD:
+      if (!record->tap.count && record->event.pressed) {
+        // Sequence is cmd space (delay 50) space
+        SEND_STRING(SS_LGUI(" ") SS_DELAY(50) SS_TAP(X_SPC));
+      }
+      return true;
+    // activates homerow
+    case F_HMRW:
+      if (!record->tap.count && record->event.pressed) {
+        // Sequence is cmd shift s
+        SEND_STRING(SS_LGUI(SS_LSFT("s")));
+      }
+      return true;
+    case SLSH:
+      if (!record->tap.count && record->event.pressed) {
+        tap_code16(KC_SLSH);
+      } else if (record->event.pressed) {
+        tap_code16(KC_BSLS);
+      }
+      return false;
+    case C_COPY:
+      if (!record->tap.count && record->event.pressed) {
+        // Sequence is cmd c
+        SEND_STRING(SS_LGUI("c"));
+      } else if (record->event.pressed) {
+        tap_code16(KC_C);
+      }
+      return false;
+    case V_PSTE:
+      if (!record->tap.count && record->event.pressed) {
+        // Sequence is cmd v
+        SEND_STRING(SS_LGUI("v"));
+      } else if (record->event.pressed) {
+        tap_code16(KC_V);
+      }
+      return false;
+    case W_CLOSE:
+      if (!record->tap.count && record->event.pressed) {
+        // Sequence is cmd w
+        SEND_STRING(SS_LGUI("w"));
+      } else if (record->event.pressed) {
+        tap_code16(KC_W);
+      }
+      return false;  
+    case ESC_CAPSLOCK:
+      if (!record->tap.count && record->event.pressed) {
+        // Press caps lock
+        tap_code16(KC_CAPS);
+      } else if (record->event.pressed) {
+        tap_code16(KC_ESC);
+      }
+      return false;
+    case Z_UNDO:
+      if (!record->tap.count && record->event.pressed) {
+        // Sequence is cmd z
+        SEND_STRING(SS_LGUI("z"));
+      } else if (record->event.pressed) {
+        tap_code16(KC_Z);
+      }
+      return false;
+    case PERC_REDO:
+      if (!record->tap.count && record->event.pressed) {
+        // Sequence is cmd shft z
+        SEND_STRING(SS_LGUI(SS_LSFT("z")));
+      } else if (record->event.pressed) {
+        tap_code16(KC_PERC);
+      }
+      return false;
+    case LCBR_URL:
+      if (!record->tap.count && record->event.pressed) {
+        // Sequence is cmd shift c
+        SEND_STRING(SS_LGUI(SS_LSFT("c")));
+      } else if (record->event.pressed) {
+        tap_code16(KC_LCBR);
+      }
+      return false;
+    case M_SCRSHT:
+      if (!record->tap.count && record->event.pressed) {
+        // Sequence is cmd shift 4
+        SEND_STRING(SS_LGUI(SS_LSFT("4")));
+      }
+      return true;
+    case M_SCRREC:
+      if (!record->tap.count && record->event.pressed) {
+        // Sequence is cmd shift 5
+        SEND_STRING(SS_LGUI(SS_LSFT("5")));
+      }
+      return true;
+    case S_SAVE:
+      if (!record->tap.count && record->event.pressed) {
+        // Sequence is cmd s
+        SEND_STRING(SS_LGUI("s"));
+      } else if (record->event.pressed) {
+        tap_code16(KC_S);
+      }
+      return false;
+    case SPC_CTRL:
+      if (!record->tap.count && record->event.pressed) {
+        // Sequence is cmd s
+        SEND_STRING(SS_LCTL(" "));
+      } else if (record->event.pressed) {
+        tap_code16(KC_SPC);
+      }
+      return false;
+  }
+  return true;
+}
+
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // keymap for default
   [0] = LAYOUT_universal(
-    KC_ESC   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                        KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_DEL   ,
-    KC_TAB   , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                        KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , S(KC_7)  ,
-    KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                                        KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_INT1  ,
-              KC_LALT,KC_LGUI,LCTL_T(KC_LNG2)     ,LT(1,KC_SPC),LT(3,KC_LNG2),                  KC_BSPC,LT(2,KC_ENT), RCTL_T(KC_LNG2),     KC_RALT  , KC_PSCR
+    KC_TAB   , KC_Q     , W_CLOSE  , KC_E            , KC_R         , KC_T           ,                                        KC_Y     , KC_U        , KC_I           , KC_O     , KC_P     , KC_DEL   ,
+    KC_ESC   , KC_A     , S_SAVE   , D_ALFD          , F_HMRW       , KC_G           ,                                        KC_H     , KC_J        , KC_K           , KC_L     , KC_SCLN  , S(KC_7)  ,
+    KC_LSFT  , Z_UNDO   , KC_X     , C_COPY          , V_PSTE       , KC_B           ,                                        KC_N     , KC_M        , KC_COMM        , KC_DOT   , KC_SLSH  , KC_INT1  ,
+               KC_LALT  , KC_OPT   , KC_LGUI         , LT(2,KC_LCTL), LT(1, KC1_BTN1),                                        KC_BSPC  , KC_SPC, _______        , _______  , KC_PSCR
   ),
 
   [1] = LAYOUT_universal(
-    _______  ,  KC_F1   , KC_F2    , KC_F3   , KC_F4    , KC_F5    ,                                         KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   , KC_F11   ,
-    _______  ,  _______ , _______  , KC_UP   , KC_ENT   , KC_DEL   ,                                         KC_PGUP  , KC_BTN1  , KC_UP    , KC_BTN2  , KC_BTN3  , KC_F12   ,
-    _______  ,  _______ , KC_LEFT  , KC_DOWN , KC_RGHT  , KC_BSPC  ,                                         KC_PGDN  , KC_LEFT  , KC_DOWN  , KC_RGHT  , _______  , _______  ,
-                  _______  , _______ , _______  ,         _______  , _______  ,                   _______  , _______  , _______       , _______  , _______
+    KC_TAB,       KC_EXLM,      KC_AT,      KC_LBRC,    KC_RBRC,       KC_PIPE,             KC_AMPR,  KC_7,     KC_8,    KC_9,    KC_ASTR,  SLSH,
+    TO(0),        KC_HASH,      KC_DLR,     KC_LPRN,    KC_RPRN,       KC_GRV,              KC_UNDS,  KC_4,     KC_5,    KC_6,    KC_PLUS,  KC_EQL,
+    KC_NO,        PERC_REDO,    KC_CIRC,    LCBR_URL,   KC_RCBR,       KC_TILD,             KC_MINS,  KC_1,     KC_2,    KC_3,    KC_0,     KC_ENTER,
+                  KC_LGUI,      KC_LGUI,    KC_LGUI,    TO(2),         KC_BTN2,             KC_SPC,   KC_BSPC,  _______, _______, KC_PSCR 
   ),
 
   [2] = LAYOUT_universal(
-    _______  ,S(KC_QUOT), KC_7     , KC_8    , KC_9     , S(KC_8)  ,                                         S(KC_9)  , S(KC_1)  , S(KC_6)  , KC_LBRC  , S(KC_4)  , _______  ,
-    _______  ,S(KC_SCLN), KC_4     , KC_5    , KC_6     , KC_RBRC  ,                                         KC_NUHS  , KC_MINS  , S(KC_EQL), S(KC_3)  , KC_QUOT  , S(KC_2)  ,
-    _______  ,S(KC_MINS), KC_1     , KC_2    , KC_3     ,S(KC_RBRC),                                        S(KC_NUHS),S(KC_INT1), KC_EQL   ,S(KC_LBRC),S(KC_SLSH),S(KC_INT3),
-                  KC_0     , KC_DOT  , _______  ,         _______  , _______  ,                   KC_DEL   , _______  , _______       , _______  , _______
-  ),
-
-  [3] = LAYOUT_universal(
-    RGB_TOG  , _______  , _______  , _______  , _______  , _______  ,                                        RGB_M_P  , RGB_M_B  , RGB_M_R  , RGB_M_SW , RGB_M_SN , RGB_M_K  ,
-    RGB_MOD  , RGB_HUI  , RGB_SAI  , RGB_VAI  , _______  , SCRL_DVI ,                                        RGB_M_X  , RGB_M_G  , RGB_M_T  , RGB_M_TW , _______  , _______  ,
-    RGB_RMOD , RGB_HUD  , RGB_SAD  , RGB_VAD  , _______  , SCRL_DVD ,                                        CPI_D1K  , CPI_D100 , CPI_I100 , CPI_I1K  , _______  , KBC_SAVE ,
-                  QK_BOOT    , KBC_RST  , _______  ,        _______  , _______  ,                   _______  , _______  , _______       , KBC_RST  , QK_BOOT
+    KC_TAB,    KC_NO,       M_SCRSHT,    KC_F14,                KC_NO,           KC_F15,                KC_NO,     KC_NO,       KC_NO,       KC_NO,      KC_NO,     KC_NO,
+    TO(0),     KC_NO,       M_SCRREC,    KC_MPRV,               KC_MPLY,         KC_MNXT,               KC_NO,     KC_NO,       KC_NO,       KC_NO,      KC_NO,     KC_NO,
+    KC_NO,     KC_NO,       KC_NO,       KC_KB_VOLUME_DOWN,     KC_KB_MUTE,      KC_KB_VOLUME_UP	,     KC_NO,     KC_NO,       KC_NO,       KC_NO,      KC_NO,     KC_NO,
+               KC_LGUI,     KC_LGUI,     KC_NO,                 TO(0),           KC_BTN2,               KC_SPC,    KC_BSPC,     _______    , _______  ,  KC_PSCR  
   ),
 };
 // clang-format on
